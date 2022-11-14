2faf8e4e79c35c31cd733338db160be627946059
Jeremy Hopwood <jeremyhopwood@vt.edu>
2022-11-14 08:56:58 -0500

    Initial release of Gauss-Newton estimation

---
+++ src/gaussNewtonCost.m
@@ -0,0 +1,138 @@
+function [J,dx,dJdalpha,d2Jdalpha2,P,dJdx] = gaussNewtonCost(xguess,thist,zhist,R,h,idxflag,params)
+%
+% Copyright (c) 2002 Mark L. Psiaki.     All rights reserved. 
+%               2022 Jeremy W. Hopwood.  All rights reserved.
+% 
+% This function computes the cost, the Gauss-Newton step, the first and
+% (approximate) second derivatives of the cost with respect to the step
+% size, alpha, the approximate estimation error covariance matrix
+% (assuming that the optimal solution has been reached for the nonlinear
+% least-squares problem), and the first derivative of the cost with
+% respect to x.
+%
+% Inputs:
+%
+%   xguess      The current guess of x.
+%
+%   thist       The Nx1 vector of measurement times or sample indices. If
+%               it is empty, take it to be an ordered index array the size
+%               of zhist.
+%
+%   zhist       The N x nz time history of measurements.
+%
+%   R           The measurement noise covariance. If it is given as a 
+%               nz x nz matrix, then it is assumed to be constant and not a
+%               function of the sample time or index. Otherwise, it may be
+%               given as a nz x nz x N array or a function handle that is a
+%               function of t (or k depending on thist).
+%
+%   hj          The name of the Matlab .m-file that contains the
+%               function which defines the measurement model h(t,x) or
+%               h(k,x). This may be a function handle string or char array.
+%               For example, if the measurement model is contained in the
+%               file hjmissile.m with the function name hjmissile, then on
+%               input to the present function hj must equal 'hjmissile' or
+%               @hjmissile, and the first line of the file hjmissile.m must
+%               have the form:
+%
+%                   function [hj,Hj,~] = hjmissile(t,x,i1stdrv,~,params)
+%
+%               where hj is the output evaluated at the current time step
+%               or sample index tj and the state vector xj. The argument
+%               i1stdrv is the method for computing the derivative of hj
+%               with respect to xj, Hj. The default value, 1 = 'Analytic',
+%               is the analytic formula for Hj. Other options may include
+%               'FiniteDifference', 'CSDA', and 'CFDM'. If it is equal to
+%               0, the Jacobian of hj is not computed and measmodel returns
+%               an empty array.
+%
+%   idxflag     A flag that tells whether (idxflag = 1) or not
+%               (idxflag = 1) to compute the other outputs besides J.
+%  
+% Outputs:
+%
+%   J           The nonlinear least-squares cost. Note that this
+%               is divided by 2 compared to the cost given in
+%               Bar-Shalom.
+%
+%   dx          The Gauss-Newton perturbation to xguess that is
+%               supposed to yield the optimal x.
+%
+%   dJdalpha    = dJ(xguess+alpha*delxgn)/dalpha evaluated at
+%               alpha = 0.
+%
+%   d2Jdalpha2  = d2J(xguess+alpha*delxgn)/dalpha2 evaluated at
+%               alpha = 0, except that the Hessian matrix d2Jdx2 
+%               used in this calculation is only the Gauss-Newton 
+%               approximation of the cost function Hessian.
+%
+%   P           The Gauss-Newton approximation of the estimation
+%               error covariance. It is the inverse of the approximate
+%               Hessian of J.
+%
+%   dJdx       The first partial derivative of J with respect to x.
+%
+
+% Get the number and dimension of the measurements and the dimension of x.
+N = size(zhist,1);
+nz = size(zhist,2);
+nx = size(xguess,1);
+
+% If thist is empty, make it the sample indices
+if isempty(thist)
+    thist = (1:N).';
+end
+
+% Pre-compute the inverse of the cholesky decompisition of R if necessary
+if ~isa(R,'function_handle') && size(R,3) == 1
+    invcholR = inv(chol(R));
+end
+
+% Loop through the measurements and set up the error vector and the
+% stacked measurement Jacobian matrix.  Re-normalize so as to deal with a
+% nonlinear measurement equation that has a measurement error with the
+% identity matrix for its covariance.
+delza = zeros(nz*N,1);
+if ~all(idxflag == 0)
+    Ha = zeros(nz*N,nx);
+end
+sidx = 1:nz;
+for k = 1:N
+    [hj,Hj,~] = feval(h,thist(k,1),xguess,idxflag,0,params);
+    zj = zhist(k,:).';
+    if ~isa(R,'function_handle')
+        if size(R,3) > 1
+            Rainvk = inv(chol(R(:,:,k)));
+        else
+            Rainvk = invcholR;
+        end
+    else
+        Rainvk = inv(chol(R(thist(k,1))));
+    end
+    delza(sidx,1) = Rainvk'*(zj - hj);
+    if ~all(idxflag == 0)
+        Ha(sidx,:) = Rainvk'*Hj;
+    end
+    sidx = sidx + nz;
+end
+
+% Compute the cost.
+J = 0.5*(delza'*delza);
+
+% Return if only the cost is needed.
+if all(idxflag == 0)
+    return
+end
+
+% Compute the Gauss-Newton search direction. Use QR factorization.
+[Qb,Rb] = qr(Ha,0);
+delzb1 = Qb'*delza;
+Rbinv = inv(Rb);
+dx = Rbinv*delzb1;
+P = Rbinv*(Rbinv');
+dJdx = Ha'*delza;
+dJdalpha = dJdx'*dx;
+dum = Rb*dx;
+d2Jdalpha2 = dum'*dum;
+
+end
---
+++ src/gaussNewtonEstimation.m
@@ -0,0 +1,178 @@
+function [xhat,Jopt,P,termflag] = gaussNewtonEstimation(thist,zhist,R,h,xguess,derivmethod,dispflag,params)
+%gaussNewtonEstimation
+%
+% Copyright (c) 2002 Mark L. Psiaki.     All rights reserved.
+%               2022 Jeremy W. Hopwood.  All rights reserved.  
+%
+% This function does nonlinear batch least-squares estimation using
+% the Gauss-Newton method.
+%
+% Inputs:
+% 
+%   t           The N x 1 vector of measurement times or sample indices. If
+%               t is given as an empty array, it is taken to be the
+%               ordered sample indices of zhist.
+%
+%   z           The N x nx time history of measurements.
+%
+%   R           The measurement noise covariance. It may be given as a
+%               constant nz x nz matrix, a nz x nz x N array, or a function
+%               handle that is a function of thist and returns a nz x nz
+%               matrix.
+%
+%   h           The name of the Matlab .m-file that contains the function
+%               which defines the measurement model. It may be given as a
+%               function handle, string, or char array. The function must
+%               have the following form:
+%
+%                   [hj,Hj,~] = measmodel(tj,xj,i1stdrv,~,params)
+%
+%               where hj is the output evaluated at the current time step
+%               or sample index tj and the state vector xj. The argument
+%               i1stdrv is the method for computing the derivative of hj
+%               with respect to xj, Hj. If it is equal to 0, the Jacobian
+%               of hj is not computed and measmodel returns an empty array.
+%               See 'derivmethod' below for more details.
+%
+%  xguess       The initial guess of x.
+%
+%  derivmethod  The method for computing the derivative of hj with
+%               respect to xj, Hj. The default value, 1 = 'Analytic', is
+%               the analytic formula for Hj. Other options may include
+%               'FiniteDifference', 'CSDA', and 'CFDM'.
+%
+%  dispflag      A flag that tells whether (dispflag = 1) or not 
+%                (dispflag = 0) to display interm optimization
+%                results during the Gauss-Newton iterations.
+%
+%  params       A constant parameter struct, array, etc that is passed to
+%               the measurement model, h.
+%  
+%  Outputs:
+%
+%  xhat         The final estimate of x.
+%
+%  Jopt         The final optimal value of the nonlinear least-squares
+%               cost.  Note that this is divided by 2 compared to the  
+%               cost given in Bar-Shalom.
+%
+%  P            The Gauss-Newton approximation of the estimation
+%               error covariance.  It is the inverse of the approximate
+%               Hessian of J.
+%
+%  termflag     A termination flag that tells whether the
+%               procedure terminated well or not.  Its output values
+%               have the following interpretations:
+%
+%                   0   Normal termination at the optimum
+%
+%                   1   Terminated because more than 50 step
+%                       size halvings were required.  This
+%                       may be an optimum.
+%
+%                   2   Terminated because more than 100
+%                       Gauss-Newton iterations were required.
+%                       This may not be the optimum.
+%
+
+% Calculate the initial cost and Gauss-Newton search direction.
+[J,delxgn,dJdalpha,d2Jdalpha2,P,~] = gaussNewtonCost(xguess,thist,zhist,R,h,derivmethod,params);
+
+% Predict the change in cost if a step size of alpha = 1 is taken.
+delJpred = dJdalpha + 0.5*d2Jdalpha2;
+
+% Decide whether to terminate now.
+delJsizetest = abs(delJpred) < 1e-13*(1 + J);
+delxsizetest = norm(delxgn) < 1e-09*(1 + norm(xguess));
+termflag = 0;
+if delJsizetest && delxsizetest
+    xhat = xguess;
+    Jopt = J;
+    return
+end
+
+% Prepare quantities for use in controlling the Gauss-Newton iterations.
+testdone = false;
+niteration = 0;
+iaflag = 0;
+
+% Do one Gauss-Newton iteration per iteration of the following loop.
+while ~testdone
+    
+    % Initial step size and Gauss-Newton iteration
+    alpha = 1;
+    xguessnew = xguess + alpha*delxgn;
+    Jnew = gaussNewtonCost(xguessnew,thist,zhist,R,h,0,params);
+
+    % Do step size halving if necessary in order to force a decrease
+    % in the cost.
+    nalphahalf = 0;
+    while Jnew >= J
+        nalphahalf = nalphahalf + 1;
+        if nalphahalf > 50
+            iaflag = 1;
+            break
+        end
+        alpha = 0.5*alpha;
+        xguessnew = xguess + alpha*delxgn;
+        Jnew = gaussNewtonCost(xguessnew,thist,zhist,R,h,0,params);
+    end
+    
+    % If we have halved the step size more than 50 times, make note.
+    if iaflag == 1
+        termflag = 1;
+        break
+    end
+
+    % Update variables for next iteration.
+    xguess = xguessnew;
+    Jold = J;
+    delJold = Jnew - J;
+    delJpredold = delJpred;
+
+    % Perform a Gauss-Newton iteration.
+    [J,delxgn,dJdalpha,d2Jdalpha2,P,~] = gaussNewtonCost(xguess,thist,zhist,R,h,derivmethod,params);
+
+    % Compute quantities in order to check whether we should terminate.
+    delJpred = dJdalpha + .5*d2Jdalpha2;
+    delJsizetest = abs(delJpred) < 1.e-13*(1 + J);
+    delxsizetest = norm(delxgn) < 1.e-09*(1 + norm(xguess));
+    alphatest = alpha == 1;
+    delJratiotest = abs((delJold/delJpredold) - 1) < 0.01;
+   
+    % Criteria #1: The change in x and J is sufficiently small. 
+    if delJsizetest && delxsizetest
+        testdone = true;
+    end
+
+    % Criteria #2: The change in x is sufficiently small, the ratio of
+    %              changes in delta J is sufficiently close to 1 for the
+    %              case where the step size has not been halved.
+    if alphatest && delJratiotest && delxsizetest
+        testdone = true;
+    end
+
+    % If we have gotten to here, we have not converged nicely to a optimal
+    % value. Perform more Gauss-Newton iterations.
+    niteration = niteration + 1;
+
+    % Criteria #3: We have succeeded the maximum number of Gauss-Newton
+    %              iterations.
+    if ~testdone && niteration >= 100
+        termflag = 2;
+        testdone = true;
+    end
+
+    % If desired, display the results of this Gauss-Newton iteration.
+    if dispflag == 1
+        fprintf('Iteration %02i : alpha = %06.4e, Jnew = %06.4e, Jold = %06.4e, norm(delxnew) = %06.4e\n',...
+            niteration,alpha,J,Jold,norm(delxgn));
+    end
+
+end
+
+% Populate necessary outputs with the results.
+xhat = xguess;
+Jopt = J;
+
+end
